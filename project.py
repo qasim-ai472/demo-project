b1da7de0abbf1e5ece127c48361957dcac9b50fd
qasim-ai472 <qasi65283@gmail.com>
2025-09-23 01:18:49 +0500

    First Project

--- project.py
+++ project.py
@@ -1,1 +1,11 @@
-print("Hello world")
+import random
+chars = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+print("Welcome to the Password Generator!")
+
+length = int(input("Enter the desired password length: "))
+password = "".join(random.sample(chars, length))
+print("Generated password:", password)
+print("Thank you for using the Password Generator!")
+
+
